a9f0f5208111fc8c36c1ad37a053d83bbf4c09a5
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-10-29 10:45:51 +0100

    Implemented post form

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
+    <form method="POST" @submit="sendItem()">
+      <input v-model="todoitem" placeholder="Enter new item"/>
+      <input type="submit" value="Submit"/>
+    </form>
     <ul>
       <li v-for="item in todolist" v-bind:key="item">{{item}}</li>
     </ul>
@@ -17,17 +21,24 @@ export default {
   data() {
     return appData;
   },
-  mounted:function() {
+  mounted: function() {
     this.getList();
   },
   methods: {
-    getList: getList
+    getList: getList,
+    sendItem: sendItem
   }
 }
 function getList() {
   axios.get("/api/lists").then( res => {
     appData.todolist = res.data.list
   });
+}
+async function sendItem() {
+  const params = new URLSearchParams();
+  params.append('item', this.todoitem);
+  await axios.post("/api/lists", params);
+  getList()
 }
 </script>
 
