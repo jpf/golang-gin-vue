abd00a58d7b1a3323dbf0f98be210b168b00e998
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-11-02 10:40:44 +0100

    Finally got the access token

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="app">
     <h1>To-Do List</h1>
-    <form method="POST" @submit="sendItem()">
-      <input v-model="todoitem" placeholder="Enter new item"/>
+    <form @submit.prevent="customLogin()">
+      Email: <input id="email" v-model="email" type="email" size="50"/>
+      Password: <input id="password" v-model="password" type="password"/>
+      <input type="submit" value="Login"/>
+    </form>
+    <form @submit.prevent="sendItem()">
+      <input type="text" size="50" v-model="todoitem" placeholder="Enter new item"/>
       <input type="submit" value="Submit"/>
     </form>
     <ul>
@@ -13,32 +18,56 @@
 
 <script>
 import axios from "axios";
+import { OktaAuth } from '@okta/okta-auth-js'
 const appData = {
-  todolist: ["More to do"]
+  todolist: ["More to do"],
+  token: ''
 }
 export default {
   name: 'App',
   data() {
     return appData;
   },
-  mounted: function() {
-    this.getList();
-  },
   methods: {
     getList: getList,
-    sendItem: sendItem
+    sendItem: sendItem,
+    customLogin: customLogin
+  },
+  mounted: function() {
+    getList();
   }
 }
+
 function getList() {
   axios.get("/api/lists").then( res => {
     appData.todolist = res.data.list
   });
 }
+
 async function sendItem() {
   const params = new URLSearchParams();
   params.append('item', this.todoitem);
   await axios.post("/api/lists", params);
   getList()
+}
+
+function customLogin() {
+  const authClient = new OktaAuth({issuer: 'https://' + process.env.VUE_APP_OKTA_DOMAIN});
+  authClient.signIn({
+    username: this.email,
+    password: this.password
+  }).then(transaction => {
+    authClient.token.getWithoutPrompt({
+      clientId: process.env.VUE_APP_OKTA_CLIENT_ID,
+      responseType: ['token'],
+      scopes: ['openid', 'email', 'profile'],
+      sessionToken: transaction.sessionToken,
+      redirectUri: window.location.origin + '/login/callback'
+    }).then(response => {
+      this.token = response.tokens.accessToken.accessToken;
+      alert('Token:' + JSON.stringify(this.token));
+    })
+  })
 }
 </script>
 
