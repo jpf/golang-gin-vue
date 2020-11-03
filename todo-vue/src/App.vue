6c967803f126086b58fb324aa29a047446843be6
Dr Phill Edwards <phill@aux-quatre-vents-france.fr>
2020-11-03 09:49:44 +0100

    Finally got sign in widget working

--- todo-vue/src/App.vue
+++ todo-vue/src/App.vue
@@ -1,91 +1,95 @@
 <template>
-  <div id="app">
-    <h1>To-Do List</h1>
-    <form @submit.prevent="customLogin()">
-      Email: <input id="email" v-model="email" type="email" size="50"/>
-      Password: <input id="password" v-model="password" type="password"/>
-      <input type="submit" value="Login"/>
-    </form>
-    <form @submit.prevent="sendItem()">
-      <input type="text" size="50" v-model="todoitem" placeholder="Enter new item"/>
-      <input type="submit" value="Submit"/>
-    </form>
-    <ul>
-      <li v-for="item in todolist" v-bind:key="item">{{item}}</li>
-    </ul>
-    <div>{{message}}</div>
-  </div>
+    <div id="app">
+        <h1>To-Do List</h1>
+        <div id="widget-container"></div>
+        <form @submit.prevent="sendItem()">
+            <input
+                type="text"
+                size="50"
+                v-model="todoitem"
+                placeholder="Enter new item"
+            />
+            <input type="submit" value="Submit" />
+        </form>
+        <ul>
+            <li v-for="item in todolist" v-bind:key="item">{{ item }}</li>
+        </ul>
+        <div>{{ message }}</div>
+    </div>
 </template>
 
 <script>
 import axios from "axios";
-import { OktaAuth } from '@okta/okta-auth-js'
+import OktaSignIn from "@okta/okta-signin-widget";
+import "@okta/okta-signin-widget/dist/css/okta-sign-in.min.css";
 const appData = {
-  todolist: ["More to do"],
-  token: '',
-  message: ''
-}
+    todolist: ["More to do"],
+    token: "",
+    message: "",
+};
 export default {
-  name: 'App',
-  data() {
-    return appData;
-  },
-  methods: {
-    getList: getList,
-    sendItem: sendItem,
-    customLogin: customLogin
-  },
-  mounted: function() {
-    getList();
-  }
-}
+    name: "App",
+    data() {
+        return appData;
+    },
+    methods: {
+        getList: getList,
+        sendItem: sendItem,
+    },
+    mounted: function () {
+        var signIn = new OktaSignIn({
+            el: "#widget-container",
+            baseUrl: "https://" + process.env.VUE_APP_OKTA_DOMAIN,
+            clientId: process.env.VUE_APP_OKTA_CLIENT_ID,
+            redirectUri: window.location.origin,
+            authParams: {
+                issuer:
+                    "https://" + process.env.VUE_APP_OKTA_DOMAIN + "/oauth2/default",
+                responseType: ["token", "id_token"],
+                display: "page",
+            },
+        });
+        signIn.renderEl({ el: "#widget-container" });
+        if (signIn.hasTokensInUrl()) {
+            signIn.authClient.token.parseFromUrl().then(function success(response) {
+                appData.token = response.tokens.accessToken.accessToken;
+                axios.defaults.headers.common["Authorization"] =
+                    "Bearer " + appData.token;
+                appData.message = appData.token;
+                signIn.hide();
+            });
+        }
+        getList();
+    },
+};
 
 function getList() {
-  axios.get("/api/lists").then( res => {
-    appData.todolist = res.data.list
-  });
+    axios.get("/api/lists").then((res) => {
+        appData.todolist = res.data.list;
+    });
 }
 
 async function sendItem() {
-  const params = new URLSearchParams();
-  params.append('item', this.todoitem);
-  await axios.post("/api/lists", params)
-  .then(function() {
-    getList();
-  })
-  .catch(function (error) {
-    appData.todolist = [ error.message ];
-  })
-}
-
-function customLogin() {
-  const authClient = new OktaAuth({issuer: 'https://' + process.env.VUE_APP_OKTA_DOMAIN});
-  authClient.signIn({
-    username: this.email,
-    password: this.password
-  }).then(transaction => {
-    authClient.token.getWithoutPrompt({
-      clientId: process.env.VUE_APP_OKTA_CLIENT_ID,
-      responseType: ['token'],
-      scopes: ['openid', 'email', 'profile'],
-      sessionToken: transaction.sessionToken,
-      redirectUri: window.location.origin + '/login/callback'
-    }).then(response => {
-      this.token = response.tokens.accessToken.accessToken;
-      axios.defaults.headers.common['Authorization'] = 'Bearer ' + this.token;
-      appData.message = this.token;
-    })
-  })
+    const params = new URLSearchParams();
+    params.append("item", this.todoitem);
+    await axios
+        .post("/api/lists", params)
+        .then(function () {
+            getList();
+        })
+        .catch(function (error) {
+            appData.todolist = [error.message];
+        });
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
 }
 </style>
